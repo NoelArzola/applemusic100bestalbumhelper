eaa71b319aef54f2ab778fe898487b1d3e32868b
Noel Arzola <noel@skillcrush.com>
2024-06-05 19:52:53 -0400

    Update to add in descending order

--- 100BestAlbumsScript.js
+++ 100BestAlbumsScript.js
@@ -1,17 +1,45 @@
-const addToLibraryButtons = document.querySelectorAll(
-  "[data-testid='add-to-library-button']"
-);
+/* 
+By default this will show in your recently added with the top albums first,
+to modify this behavior:
+  --change seedNumber to 1
+  --change the while to seedNumber <= 100
+  --change seedNumber-- to seedNumber++ 
+  --check to make sure you did that last step ;-)
+*/
 
 const delayPerButton = 15000;
+let seedNumber = 100;
 
-async function addToLibraryWithDelay(buttons, delayPerButton) {
-  for (const button of buttons) {
-    await new Promise((resolve) => setTimeout(resolve, delayPerButton));
-    button.click();
+async function addToLibraryWithDelay(seedNumber, delayPerButton) {
+  while (seedNumber > 0) {
+    const albumID = seedNumber.toString();
+    try {
+      const parentElement = document.getElementById(albumID);
+      if (parentElement) {
+        const button = parentElement.querySelector(
+          '[data-testid="add-to-library-button"]'
+        );
+        if (button) {
+          await new Promise((resolve) => setTimeout(resolve, delayPerButton));
+          button.click();
+        } else {
+          console.log(
+            `Button not found for album #${albumID}. Is it already in your library?`
+          );
+        }
+      } else {
+        console.log(
+          `Album #${albumID} not found. Did you load the whole page?`
+        );
+      }
+    } catch (error) {
+      console.log(`Error processing album # ${albumID}`, error);
+    }
+    seedNumber--;
   }
 }
 
-addToLibraryWithDelay(addToLibraryButtons, delayPerButton)
+addToLibraryWithDelay(seedNumber, delayPerButton)
   .then(() => {
     console.log("Albums added");
   })
--- README.md
+++ README.md
@@ -1,22 +1,38 @@
 # Apple Music's 100 Best Albums Helper
+
 This tool helps you easily add all 100 albums that made the list to your Apple Music library.
 
 ## Step 1 - Log in
+
 Log in to https://100best.music.apple.com using the Apple ID you want to add the albums to.
 
 ## Step 2 - Load the albums
+
 This is the boring part. To get the full 100 buttons to render, you have to scroll from 100 to 1.
+
 1. You have to click the list view in the top left for this to work
 2. Click album 100, Robyn. This will give you the expanded view that we need to expose the buttons
 3. Start scrolling. I recommend using End, Page Down, or the down arrow in that order to get from 100 to 1, this will take about a minute depending on your machine
 
 ## Step 3 - Open the console and insert the code
+
 1. Right-click on the page and click "Inspect".
 2. Click the Console tab.
 3. Paste the code at the prompt, usually indicated by `>`.
-    - If you get a warning, type "allow paste" or follow the prompt's instructions.
-    - Paste the code again.
+   - If you get a warning, type "allow paste" or follow the prompt's instructions.
+   - Paste the code again.
 
 ## Step 4 - Wait, then check
+
 In a best-case scenario, the servers will play nice, and all albums will be added. The console should output a success message.
 If it failed, or not all albums made it through, refresh the page and start at Step 2.
+
+## Modifications
+
+By default this will show in your recently added with the top albums first,
+to modify this behavior:
+
+1. Change seedNumber to 1
+2. Change the while to seedNumber <= 100
+3. Change seedNumber-- to seedNumber++
+4. Really check to make sure you did that last step
